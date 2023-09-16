0833672252b56af7951a901e475d7fb9db8c438c
loneguss <pitcha.huta@bumail.net>
2023-09-16 15:44:07 +0700

    + shoot

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 5.0f; // Adjust the forward speed as needed
+
+    void Update()
+    {
+        // Calculate the forward movement vector
+        Vector3 moveDirection = transform.forward * speed * Time.deltaTime;
+
+        // Move the object forward
+        transform.Translate(moveDirection, Space.World);
+    }
+}
