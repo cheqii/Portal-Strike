18b1cec39590a570200f32d55ddd6f7539f9dc4d
qiqitaro <thanajan.saes@bumail.net>
2023-09-28 11:55:49 +0700

    update monster data + monster patrolling

--- Assets/Script/Monster/MonsterDetectPlayer.cs
+++ Assets/Script/Monster/MonsterDetectPlayer.cs
@@ -1,16 +1,15 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class MonsterDetectPlayer : MonoBehaviour
 {
-    [Header("Monster")] 
+    #region -Declared Variables-
+
+    [Header("Monster Data")] 
     [SerializeField] private MonsterData monData;
-    [SerializeField] private NavMeshAgent monster;
-    [SerializeField] private float stopFollow;
     
+    private NavMeshAgent monster;
     private Player target;
     
     [Header("Distance between player & monster")]
@@ -18,17 +17,26 @@ public class MonsterDetectPlayer : MonoBehaviour
     private Vector2 beginPos;
     private Vector2 targetPos;
     
+    [SerializeField] private Transform centrePoint; // centre point (floor) transform for find enemy random point to patrol in the room
+
+    #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        // get component from player
         target = FindObjectOfType<Player>().GetComponent<Player>();
+        // get component NavMeshAgent from game object
+        monster = GetComponent<NavMeshAgent>();
+        // assign monster speed from monster data
+        monster.speed = monData.moveSpeed;
+        // assign monster stop distance in NavMeshAgent from monster data
+        monster.stoppingDistance = monData.stopDistance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.DrawRay(transform.position, transform.forward * monData.attackRange, Color.red);
         MoveToPlayer();
     }
 
@@ -36,32 +44,27 @@ public class MonsterDetectPlayer : MonoBehaviour
     {
         if (target == null) return;
         
-        // if distance between player and monster is lower than stop following var then monster will follow the player
-        switch (monData.monsterType)
+        if (monster.remainingDistance <= monster.stoppingDistance)
         {
-            case MonsterData.MonsterType.Melee:
-                if (awayFromPlayer <= stopFollow)
-                {
-                    monster.stoppingDistance = monData.attackRange;
-                    monster.SetDestination(target.transform.position);
-                }
-                break;
-            case MonsterData.MonsterType.Range:
-                if (awayFromPlayer <= stopFollow)
-                {
-                    monster.stoppingDistance = monData.attackRange;
-                    monster.SetDestination(target.transform.position);
-                }
-                break;
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, monData.viewRange, out point)) // pass in our centre point and radius of area
+            {
+                Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                monster.SetDestination(point);
+            }
         }
+        
+        CalculateTargetDistance();
+        // if distance between player and monster is lower than stop following var then monster will follow the player
+        if (awayFromPlayer <= monData.stopFollow) monster.SetDestination(target.transform.position);
         
         Ray ray = new Ray(transform.position, transform.forward * monData.attackRange);
         RaycastHit hit;
-        CalculateTargetDistance();
         if (Physics.Raycast(ray, out hit))
         {
             Debug.DrawRay(transform.position, transform.forward * monData.attackRange, Color.cyan);
-            if(hit.collider.gameObject.name != "Player") return;
+            if(hit.collider.gameObject.name != "Player") return; // if raycast hit something that's not player then return
+            if (monData.attackRange <= awayFromPlayer) return; // if monster attack range >= distance that monster away from player then monster can attack player
             switch (monData.monsterType)
             {
                 case MonsterData.MonsterType.Melee:
@@ -84,5 +87,20 @@ public class MonsterDetectPlayer : MonoBehaviour
         // Calculate distance between monster and player
         awayFromPlayer = Mathf.Sqrt(Mathf.Pow(targetPos.x - beginPos.x, 2) + Mathf.Pow(targetPos.y - beginPos.y, 2));
         Debug.Log(awayFromPlayer);
+    }
+
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; // random point in a sphere
+        NavMeshHit hit;
+
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas))
+        {
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 }
--- Assets/Script/Scriptables/MonsterData.cs
+++ Assets/Script/Scriptables/MonsterData.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "MonsterData", menuName = "ScripableObjects/Monster_Data", order = 1)]
+[CreateAssetMenu(fileName = "MonsterData", menuName = "ScriptableObjects/Monster_Data", order = 1)]
 public class MonsterData : ScriptableObject
 {
     public enum MonsterType
@@ -8,9 +8,10 @@ public class MonsterData : ScriptableObject
         Melee,
         Range
     }
-
+    
+    [Header("Monster Stat")]
     public MonsterType monsterType;
-
+    
     public int hp;
     
     public float moveSpeed;
@@ -20,8 +21,15 @@ public class MonsterData : ScriptableObject
     public int def;
     
     public float atkCoolDown;
-
+    
     public int expPoints;
-
+    
     public float attackRange;
+
+    [Header("NavMeshAgent Data")]
+    public float stopFollow; // to stop following player if player away than stopFollow var
+    
+    public float stopDistance; // stop distance in Navmesh
+
+    public float viewRange; // range for enemy patrolling in the room
 }
