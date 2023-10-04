7b7921b81134b3ec63a6f3f9c9a825ba8cc02cfa
qiqitaro <thanajan.saes@bumail.net>
2023-10-04 18:59:49 +0700

    enemy melee attack to player

--- Assets/Script/Monster/Enemy.cs
+++ Assets/Script/Monster/Enemy.cs
@@ -8,9 +8,12 @@ public class Enemy : MonoBehaviour,ITakeDamage
 {
     [SerializeField] private MonsterData mondata;
     [SerializeField] private int hp;
+    [SerializeField] private Transform weapon;
     [SerializeField] private Transform shootPoint;
-    public Player target;
+    private bool isAttack;
+    private Player target;
     
+    [Header("Enemy Health Bar")]
     [SerializeField] private MicroBar _microBar;
     
     void Awake()
@@ -27,7 +30,7 @@ public class Enemy : MonoBehaviour,ITakeDamage
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.DrawRay(weapon.transform.position, weapon.transform.forward * mondata.attackRange, Color.red);
     }
 
     public void TakeDamage(int dmg)
@@ -48,8 +51,15 @@ public class Enemy : MonoBehaviour,ITakeDamage
     public void MeleeAttack()
     {
         if (target == null) return;
-        Ray ray = new Ray(transform.position, transform.forward * mondata.attackRange);
-        
+        Ray ray = new Ray(weapon.transform.position, weapon.transform.forward * mondata.attackRange);
+        RaycastHit hit;
+        Rigidbody rb = weapon.GetComponent<Rigidbody>();
+        rb.AddTorque(new Vector3(0f, 30f, 0f));
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name != "Player") return;
+            rb.velocity = Vector3.zero;
+        }
     }
     
     // Range monster attack
--- Assets/Script/Monster/MonsterDetectPlayer.cs
+++ Assets/Script/Monster/MonsterDetectPlayer.cs
@@ -50,6 +50,7 @@ public class MonsterDetectPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.DrawRay(transform.position, transform.forward * monData.attackRange, Color.cyan);
         MoveToPlayer();
     }
 
@@ -80,9 +81,9 @@ public class MonsterDetectPlayer : MonoBehaviour
             case MonsterData.MonsterType.Melee:
                 if (Physics.Raycast(ray, out hit))
                 {
-                    Debug.DrawRay(transform.position, transform.forward * monData.attackRange, Color.cyan);
+                    // Debug.DrawRay(transform.position, transform.forward * monData.attackRange, Color.cyan);
                     if(hit.collider.gameObject.name != "Player") return; // if raycast hit something that's not player then return
-                    Debug.Log("Melee Attack Player");
+                    enemy.MeleeAttack();
                 }
                 break;
         }
