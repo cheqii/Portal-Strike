df3201d3b7ad6d58eeb085d8aa1b6a4d555dfd1d
qiqitaro <thanajan.saes@bumail.net>
2023-10-09 00:20:52 +0700

    new ability card image

---
+++ Assets/Card3D/Rotate.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    // The material shaders in this card must use this number
+    [Tooltip("Material Shader Mask Number")]
+    public int maskNumber = 1;
+
+    [Tooltip("Horizontal Rotation Speed")]
+    [Range(-1, 1)]
+    public float rotationSpeedH = 0.7f;
+
+    [Tooltip("Horizontal Rotation Speed")]
+    [Range(-1, 1)]
+    public float rotationSpeedV = 0.4f;
+
+    [Tooltip("Maximum Horizontal Angle")]
+    [Range(0, 60)]
+    public float angleH = 20;
+
+    [Tooltip("Maximum Vertical Angle")]
+    [Range(0, 60)]
+    public float angleV = 8;
+
+    private float rotationCounter = 0;
+    private Transform windowTransform;
+    private Transform worldTransform;
+
+    private void Awake() {
+        windowTransform = transform.GetChild(1);
+        worldTransform = transform.GetChild(2);
+
+        SetStencilMask(maskNumber);
+    }
+
+    void Update () {
+        rotationCounter += Time.deltaTime;
+        transform.eulerAngles = new Vector3(Mathf.Sin(rotationCounter * rotationSpeedV) * angleV, Mathf.Sin(rotationCounter * rotationSpeedH) * angleH, 0);
+    }
+
+    // Set the mask numbers of the shaders, so windows will fit to the card world objects
+    public void SetStencilMask(int maskNumber) {
+        this.maskNumber = maskNumber;
+
+        windowTransform.GetComponent<Renderer>().material.SetFloat("_StencilMask", maskNumber);
+        foreach (Transform worldObject in worldTransform.GetComponentInChildren<Transform>()) {
+            foreach (Material material in worldObject.GetComponent<Renderer>().materials) {
+                material.SetFloat("_StencilMask", maskNumber);
+            }
+        }
+    }
+}
---
+++ Assets/Card3D/StencilMask.shader
@@ -0,0 +1,47 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Stencils/StencilMask" {
+	Properties {
+		[IntRange] _StencilMask("Stencil Mask", Range(0, 255)) = 0
+	}
+	SubShader {
+		Tags {
+			"RenderType" = "Opaque"
+			"Queue" = "Geometry-100"
+		}
+		ColorMask 0
+		ZWrite off
+		Stencil {
+			Ref[_StencilMask]
+			Comp always
+			Pass replace
+		}
+
+		Pass {
+			CGPROGRAM
+			#include "UnityCG.cginc"
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			struct appdata {
+				float4 vertex : POSITION;
+			};
+
+			struct v2f {
+				float4 position : SV_POSITION;
+			};
+
+			v2f vert(appdata v) {
+				v2f o;
+				o.position = UnityObjectToClipPos(v.vertex);
+				return o;
+			}
+
+			half4 frag(v2f i) : COLOR {
+				return half4(1,1,0,1);
+			}
+			ENDCG
+		}
+	}
+}
