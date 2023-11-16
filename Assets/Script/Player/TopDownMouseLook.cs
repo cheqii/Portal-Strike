ca562a18e9627aab83a6dc63be4bd52e3d049547
Guitar <wachsirin.sara@gmail.com>
2023-11-16 17:19:31 +0700

    Update

--- Assets/Script/Player/TopDownMouseLook.cs
+++ Assets/Script/Player/TopDownMouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class TopDownMouseLook : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 5.0f; // Rotation speed of the character
-    [SerializeField] private LayerMask groundLayer; // Layer mask for the ground or surfaces to hit
+    //[SerializeField] private LayerMask groundLayer; // Layer mask for the ground or surfaces to hit
 
     // private Shooting shooting;
     [SerializeField] public Nf_GameEvent shootEvent;
@@ -21,7 +21,7 @@ public class TopDownMouseLook : MonoBehaviour
 
     private void Update()
     {
-        HandleMouseInput();
+        //HandleMouseInput();
         HandleStickInput();
     }
 
@@ -31,31 +31,31 @@ public class TopDownMouseLook : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
-    private void HandleMouseInput()
-    {
-        // Cast a ray from the camera to the mouse position
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+    //private void HandleMouseInput()
+    //{
+    //    // Cast a ray from the camera to the mouse position
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
-        {
-            // Get the point where the ray hits the ground
-            Vector3 targetPosition = hit.point;
+    //    if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayer))
+    //    {
+    //        // Get the point where the ray hits the ground
+    //        Vector3 targetPosition = hit.point;
 
-            // Calculate the direction from the character to the mouse cursor
-            Vector3 direction = targetPosition - transform.position;
+    //        // Calculate the direction from the character to the mouse cursor
+    //        Vector3 direction = targetPosition - transform.position;
 
-            // Ensure the character doesn't tilt upwards or downwards
-            direction.y = 0.0f;
+    //        // Ensure the character doesn't tilt upwards or downwards
+    //        direction.y = 0.0f;
 
-            // If the direction is non-zero, rotate the character to look at the mouse cursor
-            if (direction != Vector3.zero)
-            {
-                RotateCharacter(direction);
-                HandleMouseClick();
-            }
-        }
-    }
+    //        // If the direction is non-zero, rotate the character to look at the mouse cursor
+    //        if (direction != Vector3.zero)
+    //        {
+    //            RotateCharacter(direction);
+    //            HandleMouseClick();
+    //        }
+    //    }
+    //}
 
     private void HandleStickInput()
     {
@@ -74,13 +74,13 @@ public class TopDownMouseLook : MonoBehaviour
     }
 
 
-    private void HandleMouseClick()
-    {
-        // Left Mouse Click
-        if (Input.GetMouseButtonDown(0))
-        {
-            // shooting.Perform();
-            shootEvent.Raise();
-        }
-    }
+    //private void HandleMouseClick()
+    //{
+    //    // Left Mouse Click
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        // shooting.Perform();
+    //        shootEvent.Raise();
+    //    }
+    //}
 }
